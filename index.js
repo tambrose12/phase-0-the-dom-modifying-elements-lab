76fba890855e1ac8f522ecf01356032fa277177f
Taylor Ambrose <taykesby@gmail.com>
2023-01-20 20:57:58 -0500

    commit tree bf11eb4cdf8c0d8442d138be8673a553360c614e

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.append(newHeader);
+
+
